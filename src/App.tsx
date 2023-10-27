e95639b0bf44543e9aa959a9387d89aa06692f8b
CodeSandbox <support@codesandbox.io>
2023-10-27 21:34:36 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 export default function App(ctx: any) {
   console.log(ctx);
 
+  let room = [
+    {
+      key: 1,
+      color: "green"
+    }
+  ]
+let service = {
+  stripchat: [
+    status: 1,
+    uptime: 1.22,
+  
+  ],
+  chaturbate: []
+}
   return (
     <div>
       <h1>App</h1>
       <button>Login</button>
+
+     service.map(i => <div>{i}</div>)
+
     </div>
   );
 }
